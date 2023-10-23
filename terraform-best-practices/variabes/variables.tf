0c5b5b9be798bfb1f835b2944162866340f80a58
mathesh-me <matheshme679@gmail.com>
2023-10-23 21:54:54 +0530

    Variables files are added

---
+++ terraform-best-practices/variabes/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "Value for AMI"
+  default     = "ami-123456789"
+}
+
+variable "instance-type" {
+  description = "Value for Instance type"
+  default     = "t2.micro"
+}
