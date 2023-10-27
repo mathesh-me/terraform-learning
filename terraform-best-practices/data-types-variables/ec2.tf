6920139cef8b30ff1040d005289a3b1f92066846
mathesh-me <matheshme679@gmail.com>
2023-10-27 11:54:57 +0530

    terraform-best-practices/data-types-variables files are added

--- terraform-best-practices/data-types-variables/ec2.tf
+++ terraform-best-practices/data-types-variables/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance-1" {
   ami = "ami-123456789"
-  instance_type = var.instance-type
+  instance_type = var.instance-type["0"]
 
   tags = {
     Name = var.instance-name
--- terraform-best-practices/data-types-variables/variables.tf
+++ terraform-best-practices/data-types-variables/variables.tf
@@ -1,9 +1,10 @@
 variable "instance-name" {
     type = number
+    default = 98
     description = "Name for Instance"
 }
 variable "instance-type" {
-    type = list()
+    type = list(string)
     default = ["t2.micro","t2.medium","t2.large"]
     description = "Value for Instance type"
 }
