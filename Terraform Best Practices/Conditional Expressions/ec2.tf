0c171594a97c9779a974fc664cb46b16801a9518
mathesh-me <matheshme679@gmail.com>
2023-11-01 21:32:03 +0530

    Conditional Expressions Section is added

---
+++ Terraform Best Practices/Conditional Expressions/ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "dev-environment" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   count = var.istest == true ? 3 : 0
+}
+
+resource "aws_instance" "prod-environment" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.large"
+   count = var.istest == false ? 1 : 0
+}
