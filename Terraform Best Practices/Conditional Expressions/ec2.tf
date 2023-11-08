2040e29745ee0ffe8cdc97da0ed239c2f9731e80
mathesh-me <matheshme679@gmail.com>
2023-11-08 22:04:58 +0530

    Conditional Expressions/ec2.tf file id modified

--- Terraform Best Practices/Conditional Expressions/ec2.tf
+++ Terraform Best Practices/Conditional Expressions/ec2.tf
@@ -7,5 +7,6 @@ resource "aws_instance" "dev-environment" {
 resource "aws_instance" "prod-environment" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.large"
+
    count = var.istest == false ? 1 : 0
 }
