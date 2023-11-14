f0eeac9303557d342a3ce32056376990515d4209
mathesh-me <matheshme679@gmail.com>
2023-11-14 10:33:34 +0530

    create_before_destroy Argument content is added

---
+++ Terraform Best Practices/Lifecycle Meta Argument/create_before_destroy/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
