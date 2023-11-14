099e1dedb07487e28364a17fa52a981edb6a8bb8
mathesh-me <matheshme679@gmail.com>
2023-11-14 10:38:49 +0530

    ignore_changes Argument content is added

---
+++ Terraform Best Practices/Lifecycle Meta Argument/ignore_changes/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
+}
