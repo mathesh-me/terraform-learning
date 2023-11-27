ef728317e017c36fda94fe253459598ab94ea3de
mathesh-me <matheshme679@gmail.com>
2023-11-27 21:23:10 +0530

    local-exec main.tf file content is added

---
+++ Terraform Provisioners/local-exec Provisioner/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web.public_ip} > ip_address.txt"
+  }
+}
